dfa4b8f60ecae6530520a653c7282750c2595600
NulledExceptions <mmarnell1@gmail.com>
2017-03-02 01:16:25 -0600

    removing excess files

--- test/regexTest.py
+++
@@ -1,6 +0,0 @@
-from BeautifulSoup import BeautifulSoup
-import urllib2
-url =
-page = urllib2.urlopen(url)
-soup = BeautifulSoup(page.read())
-div_content = soup.find("div", {"class": "displaySection"})div class="accordionTeamSchedule
